1006eec4970b9fbbe1211dbd0d59d53b27d4ddb0
Denis Polikarpov <polikarpov.denv.2@gmail.com>
2026-03-02 19:41:13 +0300

    Edge_sense module is written

--- common/edge_sense.sv
+++ common/edge_sense.sv
@@ -1,16 +1,16 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Polikarpov D. A.
 // 
 // Create Date: 02.03.2026 18:24:23
 // Design Name: 
 // Module Name: edge_sense
-// Project Name: 
+// Project Name: common_module
 // Target Devices: 
 // Tool Versions: 
 // Description: 
-// 
+// * Module can detect edges of 1-bit signal.
 // Dependencies: 
 // 
 // Revision:
@@ -19,8 +19,32 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module edge_sense(
-
-    );
-endmodule
+module edge_sense
+#(
+    parameter EDGE_TO_DETECT = "BOTH" // "RISING" // "FALLING" // "BOTH"
+)
+(
+    input  logic i_clk,
+    input  logic i_signal,
+    output logic o_detect
+);
+    // -----------------------------------------------
+    // Declarations
+    logic signal_delayed = '0;
+    // -----------------------------------------------
+    // Basic logic
+    always_ff @(posedge i_clk) begin : delay
+        signal_delayed <= i_signal;
+    end : delay
+    // -----------------------------------------------
+    // Assign block
+    if (EDGE_TO_DETECT == "RISING") begin
+        assign o_detect = (~signal_delayed) && i_signal ? 1'b1 : 1'b0;
+    end
+    else if (EDGE_TO_DETECT == "FALLING") begin
+        assign o_detect = signal_delayed && (~i_signal) ? 1'b1 : 1'b0;
+    end
+    else if (EDGE_TO_DETECT == "BOTH") begin
+        assign o_detect = signal_delayed ^ i_signal ? 1'b1 : 1'b0;
+    end
+endmodule : edge_sense
