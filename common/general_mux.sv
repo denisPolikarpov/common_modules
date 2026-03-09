b5d2f48cd8c04c643f1a8fdc688c9adb269577eb
Denis Polikarpov <polikarpov.denv.2@gmail.com>
2026-03-09 17:43:39 +0300

    general_mux can be acync now

--- common/general_mux.sv
+++ common/general_mux.sv
@@ -7,14 +7,13 @@
 // Module Name: general_mux
 // Project Name: common_module
 // Description: 
-// * Standart synchronous mux
-// Dependencies: 
-// 
+// * General mux
 // Revision:
 // Revision 0.01 - File Created
-// Revision 0.02 - First version of module
+//          0.02 - First version of module
+//          0.03 - Async variant
 // Additional Comments:
-// 
+// * By parameter is possible to controll are general_mux is synch or async device
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -22,6 +21,7 @@ module general_mux
 #(
     parameter  int unsigned INPUT_WIDTH       = 16,
     parameter  int unsigned NUM_OF_SIGNALS    = 4,
+    parameter               SYNC_OR_ASYNC     = "SYNC",  // "SYNC"   // "ASYNC"
     localparam int unsigned CTRL_SIGNAL_WIDTH = $clog2(NUM_OF_SIGNALS)
 )
 (
@@ -30,17 +30,23 @@ module general_mux
            logic [INPUT_WIDTH       - 1 : 0] i_signals [NUM_OF_SIGNALS - 1 : 0],
     output logic [INPUT_WIDTH       - 1 : 0] o_signal
 );
-
-    always_ff @(posedge i_clk) begin : mux
-        o_signal <= i_signals[i_ctrl];
-    end : mux
-    
+    if (SYNC_OR_ASYNC == "SYNC") begin
+        always_ff @(posedge i_clk) begin : mux
+            o_signal <= i_signals[i_ctrl];
+        end : mux
+    end
+    else if (SYNC_OR_ASYNC == "ASYNC") begin
+        always_comb begin
+            o_signal <= i_signals[i_ctrl];
+        end
+    end
 endmodule : general_mux
 /*
     general_mux
     #(
-        .INPUT_WIDTH    ( 16 ),
-        .NUM_OF_SIGNALS (  4 )
+        .INPUT_WIDTH    (   16   ),
+        .NUM_OF_SIGNALS (    4   ),
+        .SYNC_OR_ASYNC  ( "SYNC" )  // "SYNC"   // "ASYNC"
     )
     general_mux_inst
     (
